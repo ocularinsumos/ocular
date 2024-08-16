260ee202862235a7d6add605e7aa0f7beca7ae34
matias <matiasrozas.lp@gmail.com>
2024-08-16 07:08:12 -0300

    maker

--- src/components/Ubicacion/Ubicacion.jsx
+++ src/components/Ubicacion/Ubicacion.jsx
@@ -1,22 +1,20 @@
 'use client';
-import { Map, Marker, Draggable} from 'pigeon-maps';
+import { Map, Marker } from 'pigeon-maps';
 import { useState } from 'react';
 
 const Mapa = () => {
-  const center = [-34.622959938473436, -58.39504191349283]; // Coordenadas de ejemplo (Nueva York, USA)
+  const center = [-34.622959938473436, -58.39504191349283]; // Coordenadas de ejemplo (Buenos Aires, Argentina)
 
   // Estado para controlar el marcador
   const [anchor, setAnchor] = useState([-34.622959938473436, -58.39504191349283]);
 
   return (
     <Map height={400} defaultCenter={center} defaultZoom={13}>
-    <Draggable offset={[60, 87]} anchor={anchor} onDragEnd={setAnchor} >
-   
+      <Marker anchor={anchor}>
         <a href="https://www.google.com/maps/dir/-34.9491129,-58.0007115/ocular+elementos+quirurgicos/@-34.623136,-58.3951626,21z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x95bccb1f8415c595:0xc60ce607280e2fa3!2m2!1d-58.3950741!2d-34.6231012?entry=ttu">
-            <img src='/images/logos/punto.webp' width={80} height={95}/>
-            </a>
-    </Draggable>
-
+          <img src='/images/logos/punto.webp' width={80} height={95} alt="Marker" />
+        </a>
+      </Marker>
     </Map>
   );
 };
