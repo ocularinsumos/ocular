8a2447be1022fb0fa6c7c64c40dea3b070ebea4e
matias <matiasrozas.lp@gmail.com>
2024-08-17 12:00:16 -0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
-'use client'
+"use client";
 import Link from "next/link";
 import Links from "../Constantes/Links";
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
 import { generateHref } from "@/Utils/generateHref";
 import Contactusform from "../Contact/Contactus";
 
@@ -13,7 +12,7 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
-  const path = usePathname();
+  const [activeLink, setActiveLink] = useState(""); // Estado para el enlace activo
   const [isVisible, setIsVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
 
@@ -28,23 +27,55 @@ export default function Navbar() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [prevScrollPos]);
 
+  const handleLinkClick = (href) => {
+    setActiveLink(href);
+  };
+
   return (
     <div className="py-[32px] lg:py-[48px]">
-      <Disclosure as="nav" className={`fixed top-0 left-0 right-0 sm:py-0 md:py-4 bg-primary text-text-tertiary border-neutral-200 backdrop-blur-sm z-20`}>
+      <Disclosure
+        as="nav"
+        className={`fixed top-0 left-0 right-0 sm:py-0 md:py-4 bg-primary text-text-tertiary border-neutral-200 backdrop-blur-sm z-20`}
+      >
         {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="relative flex h-16 items-center justify-between">
                 <div className="flex flex-1 items-center justify-start">
-                  <div className="flex flex-shrink-0 items-center ml-5 md:ml-0 mr-10 md:pl-0">
+                  <div className="flex flex-shrink-0 items-center   mr-10 ">
                     <Link href="/">
-                      <img loading='lazy' src='images/logos/OcularNav.webp' alt="Ocular" className=" w-36 lg:w-44"/>
+                      <img
+                        loading="lazy"
+                        src="images/logos/OcularNav.webp"
+                        alt="Ocular"
+                        className=" w-36 lg:w-44"
+                      />
                     </Link>
                   </div>
                   <div className="hidden md:flex md:ml-6">
                     <div className="flex space-x-5 items-center">
                       {Links.map((item) => (
-                        <Link key={item.name} href={generateHref(path, item.href)} className={classNames(item.current ? "text-text-secondary font-latoRegular" : "text-textLigth hover:text-secondary")} aria-current={item.current ? "page" : undefined}>
+                        <Link
+                          key={item.name}
+                          href={generateHref(null, item.href)}
+                          className={classNames(
+                            activeLink === item.href
+                              ? "text-white font-latoRegular relative inline-block group"
+                              : "text-textLigth relative inline-block group"
+                          )}
+                          aria-current={
+                            activeLink === item.href ? "page" : undefined
+                          }
+                          onClick={() => handleLinkClick(item.href)}
+                        >
+                          <span
+                            className={classNames(
+                              "absolute  bottom-[-1px] h-[1.5px] bg-white transition-transform duration-300 ease-out",
+                              activeLink === item.href
+                                ? "scale-x-100 inset-x-0"
+                                : "scale-x-0 group-hover:scale-x-100 inset-x-1"
+                            )}
+                          ></span>
                           {item.name.toLocaleUpperCase()}
                         </Link>
                       ))}
@@ -53,12 +84,18 @@ export default function Navbar() {
                   </div>
                   <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
                     {/* Botón de menú móvil */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-neutral-900">
+                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-blue-400 p-1">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                        <XMarkIcon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
                       ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                        <Bars3Icon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
                       )}
                     </Disclosure.Button>
                   </div>
@@ -69,8 +106,17 @@ export default function Navbar() {
             <Disclosure.Panel className="md:hidden z-50">
               <div className="space-y-1 px-4 pt-2 pb-3 min-h-screen border-t border-[#807f6c] bg-primary">
                 {Links.map((item) => (
-                  <Disclosure.Button key={item.name} as="a" href={generateHref(path, item.href)} className={classNames("text-text-tertiary block py-4 text-base font-medium border-b border-[#807f6c] hover:text-text-secondary")} aria-current={item.current ? "page" : undefined}>
-                    {item.name}
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    className={classNames(
+                      "block p-4 text-base font-medium border-b border-[#807f6c] hover:text-text-secondary",
+                      activeLink === item.href ? "bg-blue-300 text-text-secondary" : "text-text-tertiary "
+                    )}
+                    aria-current={activeLink === item.href ? "page" : undefined}
+                    onClick={() => handleLinkClick(item.href)}
+                  >
+                    {item.name.toLocaleUpperCase()}
                   </Disclosure.Button>
                 ))}
               </div>
