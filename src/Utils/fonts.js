712964eb868a893520f71ed67a17d9bd6d03ad52
matias <matiasrozas.lp@gmail.com>
2024-08-17 12:22:54 -0300

    utils fonts

---
+++ src/Utils/fonts.js
@@ -0,0 +1,6 @@
+import {Roboto_Condensed} from 'next/font/google'
+
+export const robotoCondensed = Roboto_Condensed({
+  weight: ['400'], // Puedes especificar los pesos que necesitas
+  subsets: ['latin'], // Puedes agregar otros subconjuntos si los necesitas
+});
