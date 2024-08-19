8c56acd1f8f662a888c9bab83fdccf696fe7cd6c
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-19 15:17:36 -0300

    nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,17 +12,21 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
-  const [activeLink, setActiveLink] = useState(""); // Estado para el enlace activo
+  const [activeLink, setActiveLink] = useState(""); 
   const [isVisible, setIsVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.pageYOffset;
       setIsVisible(currentScrollPos <= prevScrollPos);
       setPrevScrollPos(currentScrollPos);
+
+      // Verifica si el usuario ha hecho scroll para cambiar el fondo y el color del texto
+      setIsScrolled(currentScrollPos > 0);
     };
-  
+
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, [prevScrollPos]);
@@ -32,26 +36,42 @@ export default function Navbar() {
   };
 
   return (
-    <div className="py-8 lg:py-[48px]">
-      <Disclosure as="nav" className={`fixed top-0 left-0 right-0 sm:py-0 md:py-4 bg-primary text-text-tertiary border-neutral-200 backdrop-blur-sm z-20`}>
+    <div className="py-8 md:py-11 bg-gray-400">
+      <Disclosure 
+        as="nav" 
+        className={classNames(
+          "fixed top-0 left-0 right-0 sm:py-0 md:py-4 border-neutral-200 backdrop-blur-sm z-20 transition-colors duration-300", 
+          isScrolled ? "bg-primary text-text-tertiary backdrop-blur-none" : "bg-transparent text-text-primary"
+        )}
+      >
         {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-4 py-1 md:py-0 sm:px-6 lg:px-8">
               <div className="relative flex h-16 items-center justify-between">
                 <div className="flex flex-1 items-center justify-start">
-                  <div className="flex flex-shrink-0 items-center  mr-10 ">
+                  <div className="flex flex-shrink-0 items-center mr-10">
                     <Link href="/" className="">
-                      <img src="/images/logos/ocularNav.webp" alt="Ocular"  className="py-4" style={{ filter: 'drop-shadow(10px 10px 40px rgba(255, 255, 255))' }} aria-label="Logo Ocular" width={160}  height={'auto'} />
+                      <img src="/images/logos/ocularNav.webp" alt="Ocular" className="py-4" aria-label="Logo Ocular" width={160} height={'auto'} />
                     </Link>
                   </div>
                   <div className="hidden md:flex md:ml-6">
                     <div className="flex space-x-5 items-center">
                       {Links.map((item) => (
-                        <Link key={item.name} href={generateHref(null, item.href)} className={classNames(activeLink === item.href ? "text-white font-latoRegular relative inline-block group": "text-textLigth relative inline-block group" )} aria-current={activeLink === item.href ? "page" : undefined} onClick={() => handleLinkClick(item.href)} aria-label={item.name}>
-                          <span className={classNames("absolute  bottom-[-1px] h-[1.5px] bg-white transition-transform duration-300 ease-out", activeLink === item.href
-                                ? "scale-x-100 inset-x-0"
-                                : "scale-x-0 group-hover:scale-x-100 inset-x-1"
-                            )}></span>
+                        <Link 
+                          key={item.name} 
+                          href={generateHref(null, item.href)} 
+                          className={classNames(
+                            activeLink === item.href ? "font-latoRegular relative inline-block group" : "relative inline-block group",
+                            isScrolled ? "text-text-tertiary" : "text-text-primary"
+                          )} 
+                          aria-current={activeLink === item.href ? "page" : undefined} 
+                          onClick={() => handleLinkClick(item.href)} 
+                          aria-label={item.name}
+                        >
+                          <span className={classNames(
+                            "absolute bottom-[-1px] h-[1.5px] bg-white transition-transform duration-300 ease-out", 
+                            activeLink === item.href ? "scale-x-100 inset-x-0" : "scale-x-0 group-hover:scale-x-100 inset-x-1"
+                          )}></span>
                           {item.name.toLocaleUpperCase()}
                         </Link>
                       ))}
@@ -60,11 +80,11 @@ export default function Navbar() {
                   </div>
                   <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
                     {/* Botón de menú móvil */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-blue-400 p-1">
+                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-1">
                       <span className="sr-only">Open main menu</span>
                       {open 
-                        ? (<XMarkIcon className="block h-6 w-6" aria-label="close menu" />) 
-                        : (<Bars3Icon className="block h-6 w-6" aria-label="open menu"  />)}
+                        ? (<XMarkIcon className={classNames("block h-6 w-6", isScrolled ? "text-text-tertiary" : "text-text-primary")} aria-label="close menu" />) 
+                        : (<Bars3Icon className={classNames("block h-6 w-6", isScrolled ? "text-text-tertiary" : "text-text-primary")} aria-label="open menu" />)}
                     </Disclosure.Button>
                   </div>
                 </div>
@@ -74,10 +94,18 @@ export default function Navbar() {
             <Disclosure.Panel className="md:hidden z-50">
               <div className="space-y-1 px-4 pt-2 pb-3 min-h-screen border-t border-[#807f6c] bg-primary">
                 {Links.map((item) => (
-                  <Disclosure.Button key={item.name} as="a" className={classNames(
+                  <Disclosure.Button 
+                    key={item.name} 
+                    as="a" 
+                    className={classNames(
                       "block p-4 text-base font-medium border-b border-[#807f6c] hover:text-text-secondary",
-                      activeLink === item.href ? "bg-blue-300 text-text-secondary" : "text-text-tertiary "
-                    )} aria-current={activeLink === item.href ? "page" : undefined} onClick={() => handleLinkClick(item.href)} aria-label='' >
+                      activeLink === item.href ? "bg-blue-300 text-text-secondary" : "",
+                      isScrolled ? "text-text-tertiary" : "text-text-primary"
+                    )} 
+                    aria-current={activeLink === item.href ? "page" : undefined} 
+                    onClick={() => handleLinkClick(item.href)} 
+                    aria-label={item.name} 
+                  >
                     {item.name.toLocaleUpperCase()}
                   </Disclosure.Button>
                 ))}
--- src/components/SobreMi/SobreMi.jsx
+++ src/components/SobreMi/SobreMi.jsx
@@ -4,18 +4,25 @@ import { robotoCondensed } from "@/Utils/fonts";
 
 export default function SobreMi() {
   return (
-    <section id="nosotros" className="p-4 max-w-[780px] mx-auto">
-      <h2 className={`text-text-primary text-3xl  m-4 items-center text-center ${robotoCondensed.className}`}>{texto.nosotros.titulo}</h2>
-      <p className="text-text-secondary text-lg">{texto.nosotros.descripcion}</p>
-      <br />
-      <div id="nosotros" className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start ">
-        <div className="flex flex-col items-center text-start md:text-start md:items-start">
-          <p className="text-text-secondary text-lg indent-6">{texto.nosotros.mision}</p>
-          <br />
-          <p className="text-text-secondary text-lg indent-6">{texto.nosotros.valores}</p>
+    <section id="nosotros" className="relative z-10"> 
+      <article  className="p-4 max-w-[780px] mx-auto">
+        <h2 className={`text-text-primary text-3xl m-4 items-center text-center ${robotoCondensed.className}`}>{texto.nosotros.titulo}</h2>
+        <p className="text-text-secondary text-lg">{texto.nosotros.descripcion}</p>
+        <br />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
+          <div className="flex flex-col items-center text-start md:text-start md:items-start">
+            <p className="text-text-secondary text-lg indent-6">{texto.nosotros.mision}</p>
+            <br />
+            <p className="text-text-secondary text-lg indent-6">{texto.nosotros.valores}</p>
+          </div>
+          <img loading="lazy" width={100} style={{ filter: 'drop-shadow(0 4px 25px rgba(0, 123, 199))' }} className="hidden md:block md:w-full p-6 place-self-center z-10" src="images/sobreMi/ojo.webp" alt="ojo del logo" aria-description="ojo del logo" />
         </div>
-        <img loading="lazy" width={100} style={{ filter: 'drop-shadow(0 4px 25px rgba(0, 123, 199))' }} className="hidden md:block md:w-full p-6 place-self-center " src="images/sobreMi/ojo.webp" alt="ojo del logo" aria-description="ojo del logo" />
-      </div>
+
+        {/* Curva ondulada SVG */}
+      </article>
+        <svg viewBox="0 0 1440 150" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute bottom-[-20px] sm:bottom-[-40px] w-full md:bottom-[-40px] md:right-0 md:w-[70%] lg:bottom-[-40px] lg:right-0 lg:w-[50%] ">
+          <path d="M0 0H1440V50C1200 150 600 150 0 50V0Z" fill="#F3F5F5" />
+        </svg>
     </section>
   );
 }
