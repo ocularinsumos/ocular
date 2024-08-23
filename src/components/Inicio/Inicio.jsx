32ac0ecf31f251dc5f0123bfbb536fcaab47e58d
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-23 15:29:20 -0300

    fotos

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -4,30 +4,50 @@ import texto from '../Constantes/texto';
 
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+
   const slides = [
-    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/local_anz3wo.jpg",
-    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/contexto_apdbxv.jpg",
-    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/reunion_pahte4.jpg",
-    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435082/cotizar_q3z6vs.jpg",
-    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/salon_sfk4dg.jpg",
-    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/trabajo_yechyk.jpg"
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724436815/local_smyuls.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724436813/reunion_yi878l.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724436814/cotizar_chivhj.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724436814/contexto_t1sfop.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724436814/salon_rgxwa4.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724436814/trabajo_ec3sqt.webp"
+  ];
+
+  const SlidesMovil = [
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724437127/portada_lvtl8v.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724437127/traabajando_dbhxom.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724437126/local_v2g9hd.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724437126/atencion_d20jdh.webp",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724437127/reunion_cv6v8d.webp"
   ];
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize(); // Set the initial state
+    window.addEventListener('resize', handleResize);
+
     const interval = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % slides.length);
+      setActiveIndex((prevIndex) => (prevIndex + 1) % (isMobile ? SlidesMovil.length : slides.length));
     }, 3000); // Cambia la imagen cada 3 segundos
 
     return () => {
       clearInterval(interval); // Limpia el intervalo al desmontar el componente
+      window.removeEventListener('resize', handleResize);
     };
-  }, [slides.length]);
+  }, [isMobile, slides.length, SlidesMovil.length]);
+
+  const currentSlides = isMobile ? SlidesMovil : slides;
 
   return (
     <section id='inicio' className="bg-black">
       <div className="relative w-full">
         <div className="relative min-h-screen overflow-hidden rounded-lg md:h-96">
-          {slides.map((slide, index) => (
+          {currentSlides.map((slide, index) => (
             <div key={index} className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${index === activeIndex ? "opacity-100" : "opacity-0"}`}>
               <img src={slide} className="block w-full h-full object-cover opacity-30" alt={`Slide ${index + 1}`} aria-label={`Imagen del Local ${index + 1}`}/>
             </div>
