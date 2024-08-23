7d5fa5d4a9240ea338d2dad6de1d7b97371a77dd
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-23 12:59:36 -0300

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+  dest: "public",
+});
+
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['localhost','ocualrinsumosquirurgicos.com'], // Agrega dominios adicionales si es necesario
+        domains: ['localhost', 'ocualrinsumosquirurgicos.com', 'ocular-red.vercel.app'], // Add more domains if needed
         unoptimized: true
-      },
+    },
 };
 
-export default nextConfig;
+export default {
+    ...pwaConfig,
+    ...nextConfig,
+};
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,14 @@ import PreguntasFrecuentes from "@/components/PreguntasFrecuentas/PreguntasFrecu
 import Ubicacion from "@/components/Ubicacion/Ubicacion";
 import VideoPublicidad from "@/components/Video/VideoPublicidad";
 import Productos from "@/components/Productos/Productos";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
       <>
+        <Link href="#productos" className="skip-link absolute left-0 bg-gray-900 text-white p-2 rounded-md text-base font-medium -top-10 focus:top-0 focus:z-50">
+        Saltar al contenido principal
+        </Link>
         <Inicio />
         <Productos /> 
         <VideoPublicidad />
