a0a99f7dbab4decc19d622a5fdc6a3bcbdf86fe5
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-23 14:59:25 -0300

    md:images inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -5,10 +5,12 @@ import texto from '../Constantes/texto';
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const slides = [
-    "images/inicio/portada.webp",
-    "images/404/404.webp",
-    "images/inicio/img-inicio.webp",
-    "images/inicio/portada.webp"
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/local_anz3wo.jpg",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/contexto_apdbxv.jpg",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/reunion_pahte4.jpg",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435082/cotizar_q3z6vs.jpg",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/salon_sfk4dg.jpg",
+    "https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724435081/trabajo_yechyk.jpg"
   ];
 
   useEffect(() => {
@@ -27,15 +29,15 @@ const Carousel = () => {
         <div className="relative min-h-screen overflow-hidden rounded-lg md:h-96">
           {slides.map((slide, index) => (
             <div key={index} className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${index === activeIndex ? "opacity-100" : "opacity-0"}`}>
-              <img src={slide} className="block w-full h-full object-cover opacity-30" alt={`Slide ${index + 1}`} />
+              <img src={slide} className="block w-full h-full object-cover opacity-30" alt={`Slide ${index + 1}`} aria-label={`Imagen del Local ${index + 1}`}/>
             </div>
           ))}
 
           {/* Text overlay */}
           <div className="absolute inset-0 flex justify-center items-center bottom-16 text-white z-10">
             <div className="text-center">
-              <p className="text-[40px] md:text-[56px] tracking-tight text-customBeige leading-none font-playfair-regular-italic">{texto.inicio.titulo}</p>
-              <p className="text-[15px] md:text-[20px] tracking-wide text-customBeige font-light font-lato">{texto.inicio.subtitulo}</p>
+              <p className="text-[40px] md:text-[62px] tracking-tight text-customBeige leading-none font-playfair-regular-italic"><strong>{texto.inicio.titulo}</strong></p>
+              <p className="text-[20px] md:text-[22px] tracking-wide text-customBeige font-light font-lato">{texto.inicio.subtitulo}</p>
             </div>
           </div>
         </div>
