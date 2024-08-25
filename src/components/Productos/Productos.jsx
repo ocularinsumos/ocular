9a15dd1aac5f699337925cfd17572557b8600454
matias <matiasrozas.lp@gmail.com>
2024-08-25 02:24:16 -0300

    productos

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -8,14 +8,17 @@ export default function Productos({producto,texto}) {
   const locale = useLocale()
   return (
     <div id="productos">
-      <div className="my-10 md:my-20 text-center">
+      <div className="my-10 md:my-10 text-center">
         <h2 className={`text-3xl md:text-[60px] leading-[80px] text-text-primary font-normal italic px-2 mx-auto pt-3 font-playfair ${robotoCondensed.className}`}>{texto.titulo}</h2>
       </div>
-      <div className="flex justify-evenly my-10 md:px-0 flex-wrap md:mx-10">
+      <div className="flex justify-around my-10 md:px-0 flex-wrap md:mx-10">
         {producto.map((servicio, i) => (
-          <Link href={`/${locale}/${servicio.id}`} key={i} className="flex flex-col items-center text-center w-1/2 md:w-1/4 mb-10 transform transition-transform duration-300 hover:scale-105 hover:shadow-lg active:scale-110 active:duration-75">
-            <img loading="lazy" width={150} height={150} src={servicio.img} alt={servicio.title} className="w-fit px-2 md:p-5"/>
-            <h2 className="text-black mt-6">{servicio.title.toUpperCase()}</h2>
+          <Link 
+          href={`/${locale}/${servicio.id}`} key={i} 
+          className="flex flex-col mx-2 shadow-md items-center text-center w-40 md:w-36 lg:w-44 xl:w-60 mb-10 transform transition-transform duration-300 hover:scale-110 hover:shadow-xl active:scale-110 active:duration-75 rounded-lg  bg-primary  "
+          >
+            <img loading="lazy" width={150} height={150} src={servicio.img} alt={servicio.title} className="w-fit rounded-t-md"/>
+            <h2 className="text-background-primary p-2 ">{servicio.title.toUpperCase()}</h2>
           </Link>
         ))}
       </div>
