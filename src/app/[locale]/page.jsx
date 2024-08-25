1f2f93d6c7740c5746fa078b66aa9a54d87a647b
matias <matiasrozas.lp@gmail.com>
2024-08-25 01:49:55 -0300

    merge

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -4,7 +4,6 @@ import PreguntasFrecuentes from "@/components/PreguntasFrecuentas/PreguntasFrecu
 import Ubicacion from "@/components/Ubicacion/Ubicacion";
 import VideoPublicidad from "@/components/Video/VideoPublicidad";
 import Productos from "@/components/Productos/Productos";
-import Link from "next/link";
 import {getMessages} from 'next-intl/server';
 
 
@@ -13,9 +12,6 @@ export default async function HomePage() {
 
   return (
       <>
-        <Link href="#productos" className="skip-link absolute left-0 bg-gray-900 text-white p-2 rounded-md text-base font-medium -top-10 focus:top-0 focus:z-50">
-        Saltar al contenido principal
-        </Link>
         <Inicio inicio={messages.inicio}/>
         <Productos producto={messages.producto} texto={messages.productos}/> 
         <VideoPublicidad />
