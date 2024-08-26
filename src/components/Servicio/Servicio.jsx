9502c18b54b856944746a0b3392c5d21b65bcdab
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-26 14:19:57 -0300

    card

--- src/components/Servicio/Servicio.jsx
+++ src/components/Servicio/Servicio.jsx
@@ -10,7 +10,7 @@ export default function Servicio({ titulo, texto, imagen, texto2, texto3, icono,
   console.log('productos:', prod);
 
   return (
-    <section className="min-h-screen grid col-span-1 justify-items-center md:grid-cols-3 md:pl-20 md:pr-10 py-16 gap-10 md:gap-20 ">
+    <section className="min-h-screen grid col-span-1 justify-items-center md:grid-cols-3 md:px-4 lg:pl-20 lg:pr-10 py-16 gap-10 lg:gap-20 ">
       <article className="flex flex-col grid-cols-1 justify-between text-center px-6 md:px-6 pt-10 lg:pr-10">
         <div>
           <div className="flex justify-center items-center">
@@ -32,15 +32,15 @@ export default function Servicio({ titulo, texto, imagen, texto2, texto3, icono,
           </div>
         </div>
       </article>
-      <article className="relative w-full col-span-2 p-2"> 
+      <article className="relative w-full col-span-1 md:col-span-2 p-2"> 
   <ul className="space-y-4">
     {prod && prod.map((item, index) => (
       <li key={index} className="flex flex-row items-center bg-primary border hover:bg-primary-hover border-gray-200 rounded-lg shadow max-h-full mx-auto max-w-4xl">
         <img className="m-2 md:p-0 object-cover w-32 h-32 md:w-48 md:h-48 rounded-md" src={item.imagen} alt={item.descripcion} />
-        <div className="flex flex-col justify-between p-4 leading-normal w-full ">
-          <h2 className="mb-2 text-base md:text-2xl font-bold tracking-tight text-gray-300">{item.nombre}</h2>
-          <p className="mb-2 text-sm md:text-base font-normal text-gray-300">{item.descripcion}</p>
-          <p className="mb-2 text-sm md:text-base font-normal text-gray-300">{item.marca}</p>
+        <div className="flex flex-col justify-between py-2 md:p-4 leading-normal w-full ">
+          <h2 className="md:mb-2 text-base md:text-2xl font-bold tracking-tight text-gray-300">{item.nombre}</h2>
+          <p className="md:mb-2 text-xs lg:text-base font-normal text-gray-300">{item.descripcion}</p>
+          <p className="md:mb-2 text-sm md:text-base font-normal text-gray-300">{item.marca}</p>
           <small className="text-xs md:text-sm font-normal text-gray-300">{item.codigo}</small>
         </div>
       </li>
