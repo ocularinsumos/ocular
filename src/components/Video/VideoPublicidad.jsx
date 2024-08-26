4ad4bd2cbae77f189742b218821a2e2806e6b6a2
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2024-08-25 22:08:07 -0300

    video sin audio

--- src/components/Video/VideoPublicidad.jsx
+++ src/components/Video/VideoPublicidad.jsx
@@ -5,7 +5,7 @@ const VideoPublicidad = () => {
 
   return (
     <section className="relative w-full h-full mx-auto max-w-5xl overflow-hidden rounded-lg shadow-lg">
-      <video className="w-full h-auto" src='https://res.cloudinary.com/dz2c7o9z6/video/upload/v1724619242/Publi_t0owuj.mp4' alt='video publicidad' aria-label='video publicidad de oftalmologia' loop autoPlay muted playsInline />
+      <video className="w-full h-auto" src='https://res.cloudinary.com/dz2c7o9z6/video/upload/v1724634374/sin_audio_ayjkey.mp4' alt='video publicidad' aria-label='video publicidad de oftalmologia' loop autoPlay muted playsInline />
     </section>
   );
 };
