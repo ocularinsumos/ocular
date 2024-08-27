23b63c94fe10eae9885ba3dd2af2dd2e82d8dd7c
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-27 09:47:07 -0300

    info

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 import { FaWhatsapp } from "react-icons/fa";
 import { EnvelopeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 function TopBanner() {
   const t = useTranslations("TopBanner");
@@ -17,27 +18,14 @@ function TopBanner() {
 
   return (
     <section className="flex  items-center md:justify-center justify-around bg-primary min-h-10 lg:min-h-5 content-center text-white py-1 px-6 text-center w-full fixed top-0 z-30">
-      <a
-        href={`tel:+${c("codigoPais")}${c("contact")}`}
-        className="text-xs md:text-base lg:text-lg font-semibold pr-2 md:pr-10"
-      >
-        {bannerText}
-      </a>
+      <Link href={`tel:+${c("codigoPais")}${c("contact")}`} className="text-xs md:text-base lg:text-lg font-semibold pr-2 md:pr-10">{bannerText}</Link>
       <div className="flex">
-        <a
-          className=" items-center text-dark-600 text-slate-800 border-l border-white md:px-10 px-2"
-          href={`https://wa.me/+${c("codigoPais")}${c(
-            "contact"
-          )}?text=${encodeURIComponent(c("textBoton"))}`}
-        >
+        <Link className=" items-center text-dark-600 text-slate-800 border-l border-white md:px-10 px-2" target="_blank" referrerPolicy="nonreferer" href={`https://wa.me/+${c("codigoPais")}${c("contact")}?text=${encodeURIComponent(c("textBoton"))}`}>
           <FaWhatsapp className="w-5 h-5 text-text-tertiary" />
-        </a>
-        <a
-          className=" items-center  text-dark-600 text-slate-800  border-l border-white md:pl-10 pl-2"
-          href={`mailto:${t("email")}`}
-        >
+        </Link>
+        <Link className=" items-center  text-dark-600 text-slate-800  border-l border-white md:pl-10 pl-2" href={`mailto:${t("email")}`}>
           <EnvelopeIcon className="w-5 h-5 text-text-tertiary" />
-        </a>
+        </Link>
       </div>
     </section>
   );
--- src/components/Ubicacion/Ubicacion.jsx
+++ src/components/Ubicacion/Ubicacion.jsx
@@ -1,59 +1,53 @@
 "use client";
 import { EnvelopeIcon, MapPinIcon } from "@heroicons/react/24/solid";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 import { FaPhone, FaWhatsapp } from "react-icons/fa";
 
 const Ubicacion = () => {
   const t = useTranslations('userData')
   return (
-    <div className="flex flex-col items-center">
-      <section
-        id="ubicacion"
-        className="w-full bg-background-secondary"
-      >
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d205.20317250079708!2d-58.39513046627046!3d-34.62310290000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f75.1!3m3!1m2!1s0x95bccb1f8415c595%3A0xc60ce607280e2fa3!2sOcular%20Insumos%20Quir%C3%BArgicos!5e0!3m2!1sen!2sar!4v1723828574394!5m2!1sen!2sar"
-          className="w-full h-64 sm:h-80 md:h-96 lg:h-[450px] border-0 max-w-[780px] mx-auto pt-10"
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Ubicacion de la empresa"
-          aria-label="Ubicacion de la empresa"
-        />
-      </section>
-      <div className="bg-white w-full">
-        
-      <div className="bg-white w-full max-w-[780px] mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-2 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
-        <div>
-          <h2 className="font-lato text-primary">CONTACTAME</h2>
-          <div className="flex items-center mt-8 space-x-2 text-dark-600 text-slate-800">
-            <MapPinIcon className="w-5 h-5 text-primary" />
-            <span>{t('ubicacion')}</span>
-          </div>
+      <section id="ubicacion" className="w-full bg-background-secondary">
+        <article className="flex flex-col items-center">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d205.20317250079708!2d-58.39513046627046!3d-34.62310290000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f75.1!3m3!1m2!1s0x95bccb1f8415c595%3A0xc60ce607280e2fa3!2sOcular%20Insumos%20Quir%C3%BArgicos!5e0!3m2!1sen!2sar!4v1723828574394!5m2!1sen!2sar"
+            className="w-full h-64 sm:h-80 md:h-96 lg:h-[450px] border-0 max-w-[780px] mx-auto pt-10"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Ubicacion de la empresa"
+            aria-label="Ubicacion de la empresa"
+          />
+      </article>
+      <article className="bg-white w-full">
+        <div className="bg-white w-full max-w-[780px] mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-2 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
+          <div>
+            <h2 className="font-lato text-primary">CONTACTAME</h2>
 
-          <div className="flex items-center mt-2 space-x-2 text-dark-600 text-slate-800">
-            <EnvelopeIcon className="w-5 h-5 text-primary" />
-            <a href={`mailto:${t('email')}`}>{t('email')}</a>
-          </div>
+            <button className="flex items-center mt-8 space-x-2 text-dark-600 text-slate-800">
+              <MapPinIcon className="w-5 h-5 text-primary" /><span>{t('ubicacion')}</span>
+            </button>
 
-          <div className="flex items-center mt-2 space-x-2 text-dark-600 text-slate-800">
-            <FaPhone className="w-5 h-5 text-primary" />
-            <a href={`tel:+${t('codigoPais')}${t('contact')}`}>{t('numContact')}</a>
-          </div>
+            <button className="flex items-center mt-2 space-x-2 text-dark-600 text-slate-800" onClick={() => window.location.href = `mailto:${t('email')}`}>
+              <EnvelopeIcon className="w-5 h-5 text-primary" /><span>{t('email')}</span>
+            </button>
 
-          <div className="flex items-center mt-2 space-x-2 text-dark-600 text-slate-800">
-            <FaWhatsapp className="w-5 h-5 text-primary" />
-            <a href={`https://wa.me/+${t('codigoPais')}${t('contact')}?text=${encodeURIComponent(t('textBoton'))}`}>WhatsApp</a>
+            <button className="flex items-center mt-2 space-x-2 text-dark-600 text-slate-800" onClick={() => window.location.href = `tel:+${t('codigoPais')}${t('contact')}`}>
+              <FaPhone className="w-5 h-5 text-primary" /><span>{t('numContact')}</span>
+            </button>
+
+            <button className="flex items-center mt-2 space-x-2 text-dark-600 text-slate-800" onClick={() => window.open(`https://wa.me/+${t('codigoPais')}${t('contact')}?text=${encodeURIComponent(t('textBoton'))}`, '_blank', 'noopener,noreferrer')}>
+              <FaWhatsapp className="w-5 h-5 text-primary" /><span>WhatsApp</span>
+            </button>
+          </div>
+          <div>
+            <p className="max-w-sm mt-12 mb-4 text-slate-800">
+            En Ocular, nos dedicamos a ofrecerte productos de primera calidad para todas tus necesidades quirúrgicas. Sabemos lo importante que es contar con insumos confiables, por eso estamos acá para asesorarte y resolver cualquier duda que tengas
+            </p>
           </div>
         </div>
-        <div>
-          <p className="max-w-sm mt-12 mb-4 text-slate-800">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iusto, laborum, voluptatum voluptas, voluptates quia temporibus assumenda repudiandae voluptatibus odit eos perferendis aliquid eum illo sapiente sequi dignissimos tempora excepturi.
-          </p>
-        </div>
-      </div>
-    </div>
-    </div>
+      </article>
+    </section>
   );
 };
 
