62b05da81da2d7791ca47b70c48d52d31be97e3c
Gonzalo <gonzalotorresgrau@gmail.com>
2024-09-25 14:52:38 -0300

    Selector de idioma

--- src/components/Navbar/SelectIdioma/SelectIdioma.jsx
+++ src/components/Navbar/SelectIdioma/SelectIdioma.jsx
@@ -1,33 +1,26 @@
-
-
 'use client';
 
 import { useLocale } from 'next-intl';
-import { useRouter, usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
-import { useTransition } from 'react';
 
 export default function LanguageSelector() {
-  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
-  const [isPending, startTransition] = useTransition();
   const currentLocale = useLocale();
-  const pathname = usePathname().slice(3);
-  
+  const pathname = usePathname().slice(3); // Remover el idioma actual de la URL
+
   const handleLocaleChange = (locale) => {
     setIsOpen(false); // Cierra el menú después de seleccionar un idioma
-    startTransition(() => {
-      router.replace(`/${locale}${pathname}`);
-    });
+    window.location.href = `/${locale}${pathname}`; // Forzar la recarga completa con el nuevo idioma
   };
 
   return (
     <div className="relative inline-block">
-      <button id="languageDropdown" onClick={() => setIsOpen(!isOpen)} className="flex items-center px-4 py-2 bg-background-primary text-primary  rounded-md " aria-label='cambiar idioma'>
+      <button id="languageDropdown" onClick={() => setIsOpen(!isOpen)} className="flex items-center px-4 py-2 bg-background-primary text-primary rounded-md" aria-label="cambiar idioma">
         <img src={currentLocale === 'en'
               ? 'https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724675128/Eeuu_ulugsa.svg'
               : 'https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724675128/Argentina_v235v9.svg'
-          } alt={currentLocale === 'en' ? 'US Flag' : 'Argentina Flag'} className="w-5 h-3.5 mr-2" width={20} height={20} aria-label={currentLocale === 'en' ? 'US Flag' : 'Argentina Flag'} title={currentLocale === 'en' ? 'US Flag' : 'Argentina Flag'}/>
+          } alt={currentLocale === 'en' ? 'US Flag' : 'Argentina Flag'} className="w-5 h-3.5 mr-2" width={20} height={20} aria-label={currentLocale === 'en' ? 'US Flag' : 'Argentina Flag'} title={currentLocale === 'en' ? 'US Flag' : 'Argentina Flag'} />
         {currentLocale === 'en' ? 'EN' : 'ES'}
         <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
@@ -37,14 +30,14 @@ export default function LanguageSelector() {
         <div id="languageOptions" className="absolute right-0 w-full mt-2 origin-top-right bg-background-primary border border-gray-200 rounded-md shadow-lg z-10">
           <ul className="py-1">
             <li>
-              <button onClick={() => handleLocaleChange('en')} className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" aria-label='change to English'>
-                <img src="https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724675128/Eeuu_ulugsa.svg" alt="US Flag" width={20} height={20} className="w-5 h-5 mr-2"   aria-label='Selector idioma US' title='Selector idioma US'/>
+              <button onClick={() => handleLocaleChange('en')} className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" aria-label="change to English">
+                <img src="https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724675128/Eeuu_ulugsa.svg" alt="US Flag" width={20} height={20} className="w-5 h-5 mr-2" aria-label="Selector idioma US" title="Selector idioma US" />
                 EN
               </button>
             </li>
             <li>
-              <button onClick={() => handleLocaleChange('es')} className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" aria-label='cambiar a español'>
-                <img src="https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724675128/Argentina_v235v9.svg" alt="Argentina Flag" width={20} height={20} className="w-5 h-3.5 mr-2" aria-label='Selector idioma AR' title='Selector idioma AR'/>
+              <button onClick={() => handleLocaleChange('es')} className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" aria-label="cambiar a español">
+                <img src="https://res.cloudinary.com/dz2c7o9z6/image/upload/v1724675128/Argentina_v235v9.svg" alt="Argentina Flag" width={20} height={20} className="w-5 h-3.5 mr-2" aria-label="Selector idioma AR" title="Selector idioma AR" />
                 ES
               </button>
             </li>
--- src/components/PreguntasFrecuentas/PreguntasFrecuentas.jsx
+++ src/components/PreguntasFrecuentas/PreguntasFrecuentas.jsx
@@ -22,50 +22,50 @@ const PreguntasFrecuentes = () => {
   
 
   return (
-    <div className="bg-background-secondary py-8 px-4  sm:py-16 lg:px-6 overflow-y-auto max-auto " style={{textAlign:"-webkit-center"}}>
-      <div className="max-w-screen-md" >
-        <div id="accordion-open" data-accordion="open">
-          <h2 className={`text-text-primary text-3xl  m-4 items-center text-center ${robotoCondensed.className}`}>{t.preguntas.titulo}</h2>
-          {t.faqData.map((faq) => (
-            <div key={faq.id} className="mb-4 border border-gray-200 rounded-xl">
-              <h2 id={`accordion-open-heading-${faq.id}`}>
-                <button type="button" className="flex items-center justify-between w-full p-4 font-medium text-gray-600 bg-gray-50 rounded-t-xl focus:outline-none" onClick={() => toggleAccordion(faq.id)}
-                  aria-expanded={accordionOpen[faq.id]} aria-controls={`accordion-open-body-${faq.id}`}>
-                  <span className="flex items-center">
-                    <svg className={`w-5 h-5 mr-2 transition-transform ${accordionOpen[faq.id] ? "rotate-180 text-text-primary-active" : "" }`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+      <section className="bg-background-secondary py-8 px-4  sm:py-16 lg:px-6 overflow-y-auto max-auto " id="preguntas" style={{textAlign:"-webkit-center"}}>
+        <div className="max-w-screen-md" >
+          <div id="accordion-open" data-accordion="open">
+            <h2 className={`text-text-primary text-3xl  m-4 items-center text-center ${robotoCondensed.className}`}>{t.preguntas.titulo}</h2>
+            {t.faqData.map((faq) => (
+              <div key={faq.id} className="mb-4 border border-gray-200 rounded-xl">
+                <h2 id={`accordion-open-heading-${faq.id}`}>
+                  <button type="button" className="flex items-center justify-between w-full p-4 font-medium text-gray-600 bg-gray-50 rounded-t-xl focus:outline-none" onClick={() => toggleAccordion(faq.id)}
+                    aria-expanded={accordionOpen[faq.id]} aria-controls={`accordion-open-body-${faq.id}`}>
+                    <span className="flex items-center">
+                      <svg className={`w-5 h-5 mr-2 transition-transform ${accordionOpen[faq.id] ? "rotate-180 text-text-primary-active" : "" }`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+                      </svg>
+                      {faq.question}
+                    </span>
+                    <svg className={`w-5 h-5 ml-2 transition-transform ${accordionOpen[faq.id] ? "" : "rotate-180"}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={accordionOpen[faq.id]? "M19 9l-7 7-7-7" : "M5 15l7-7 7 7"} />
                     </svg>
-                    {faq.question}
-                  </span>
-                  <svg className={`w-5 h-5 ml-2 transition-transform ${accordionOpen[faq.id] ? "" : "rotate-180"}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={accordionOpen[faq.id]? "M19 9l-7 7-7-7" : "M5 15l7-7 7 7"} />
-                  </svg>
-                </button>
-              </h2>
-              <div id={`accordion-open-body-${faq.id}`} className={`${accordionOpen[faq.id] ? "block" : "hidden" } bg-white border-t border-gray-200  rounded-b-xl`} aria-labelledby={`accordion-open-heading-${faq.id} `} >
-                <div className="p-4">
-                  <p className="mb-2 text-gray-700">{faq.answer}</p>
-                  {faq.linkText && (
-                    <p className="mb-2 text-gray-700">
-                      <Link href={faq.linkUrl} className="text-blue-600 hover:underline" target="_blank" title={faq.linkUrl?faq.linkText:''} >{faq.linkUrl?faq.linkText:''}</Link>
-                    </p>
-                  )}
-                  {faq.additionalInfo && (
-                    <div>
-                      <p className="mb-2 text-gray-700">{faq.additionalInfo[0].text}</p>
-                      <ul className="pl-4 text-gray-700 list-disc">{faq.additionalInfo[0].links.map((link, index) => (
-                          <li key={index}><Link href={link.url} className="text-blue-600 hover:underline" title={link.text}>{link.text}</Link> </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
+                  </button>
+                </h2>
+                <div id={`accordion-open-body-${faq.id}`} className={`${accordionOpen[faq.id] ? "block" : "hidden" } bg-white border-t border-gray-200  rounded-b-xl`} aria-labelledby={`accordion-open-heading-${faq.id} `} >
+                  <div className="p-4">
+                    <p className="mb-2 text-gray-700">{faq.answer}</p>
+                    {faq.linkText && (
+                      <p className="mb-2 text-gray-700">
+                        <Link href={faq.linkUrl} className="text-blue-600 hover:underline" target="_blank" title={faq.linkUrl?faq.linkText:''} >{faq.linkUrl?faq.linkText:''}</Link>
+                      </p>
+                    )}
+                    {faq.additionalInfo && (
+                      <div>
+                        <p className="mb-2 text-gray-700">{faq.additionalInfo[0].text}</p>
+                        <ul className="pl-4 text-gray-700 list-disc">{faq.additionalInfo[0].links.map((link, index) => (
+                            <li key={index}><Link href={link.url} className="text-blue-600 hover:underline" title={link.text}>{link.text}</Link> </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
   );
 };
 
